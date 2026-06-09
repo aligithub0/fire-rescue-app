4604c97f6da9e8c977064db51a42210aa429aabc
Ali Ather <aliather617@gmail.com>
2026-06-10 03:45:16 +0500

    Refactor code structure for improved readability and maintainability

--- fire_fightings.sql
+++ fire_fightings.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2022 at 11:03 AM
--- Server version: 10.4.16-MariaDB
--- PHP Version: 7.4.12
+-- Generation Time: Mar 16, 2026 at 07:06 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fire_fighting`
+-- Database: `fire_fightings`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +32,7 @@ CREATE TABLE `admin` (
   `admin_username` varchar(100) NOT NULL,
   `admin_password` varchar(20) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin`
@@ -57,14 +57,14 @@ CREATE TABLE `complaint` (
   `building` varchar(200) NOT NULL,
   `status` varchar(10) NOT NULL DEFAULT 'new',
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `complaint`
 --
 
 INSERT INTO `complaint` (`complaint_id`, `station_id`, `complaint_latitude`, `complaint_longitude`, `complaint_image`, `contact`, `building`, `status`, `timestamp`) VALUES
-(23, 3, '31.3832484', '73.1254275', '62b5724142e2e.png', '03368655492', '', 'new', '2022-06-24 08:13:53');
+(48, 8, '31.44418128344532', '74.31243649358582', '69b837e2da6c8.png', '', '', 'new', '2026-03-16 17:03:30');
 
 -- --------------------------------------------------------
 
@@ -77,16 +77,15 @@ CREATE TABLE `posts` (
   `post_detail` text NOT NULL,
   `post_image` varchar(100) NOT NULL,
   `post_date` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
 INSERT INTO `posts` (`post_id`, `post_detail`, `post_image`, `post_date`) VALUES
-(1, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat temporibus, distinctio repellendus labore, ducimus unde dignissimos fugiat ipsa! Ipsum necessitatibus nam deserunt, illo. Fugiat aspernatur distinctio, unde voluptatum sit, doloremque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat temporibus, distinctio repellendus labore, ducimus unde dignissimos fugiat ipsa! Ipsum necessitatibus nam deserunt, illo. Fugiat aspernatur distinctio, unde volu', '1655645668.png', '2022-06-19'),
-(2, 'Ipsum necessitatibus nam deserunt, illo. Fugiat aspernatur distinctio, unde voluptatum sit, doloremque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat temporibus, distinctio repellendus labore, ducimus unde dignissimos fugiat ipsa! Ipsum necessitatibus nam deserunt, illo. \"\"', '1656003478.png', '2022-06-19'),
-(3, 'Be care fhf wehdw ewd', '1656060775.jpg', '2022-06-24');
+(3, 'Fire Fighting', '1656060775.jpg', '2026-03-16'),
+(4, 'The image captures a devastating fire engulfing multiple stories of a building. Flames are intensely visible, erupting from several windows and consuming the interior. Thick, black smoke billows upwards, obscuring the upper parts of the structure and indicating a significant amount of burning material. ', '1747381416.jpg', '2026-03-16');
 
 -- --------------------------------------------------------
 
@@ -105,14 +104,14 @@ CREATE TABLE `reports` (
   `report_date` varchar(20) NOT NULL,
   `complete_detail` text NOT NULL,
   `report_timestamp` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `reports`
 --
 
 INSERT INTO `reports` (`report_id`, `station_id`, `case_name`, `deaths`, `city`, `location_name`, `year`, `report_date`, `complete_detail`, `report_timestamp`) VALUES
-(3, 1, 'Satyana D Ground Road ', '2', 'Toba Tek Singh', 'HBL bank ', 2022, '2022-06-16', 'ore deserunt hic? Obcaecati voluptatum aspernatur ut delectus adipisci vero sunt eveniet, ', '2022-06-21 14:47:20');
+(4, 8, 'Fire in building', '6', 'Lahore', 'Rana Road Township', '2023', '2026-03-16', 'Report is completed.', '2026-03-16 08:30:26');
 
 -- --------------------------------------------------------
 
@@ -126,7 +125,7 @@ CREATE TABLE `rescue_team` (
   `team_name` varchar(200) NOT NULL,
   `team_username` varchar(200) NOT NULL,
   `team_password` varchar(111) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `rescue_team`
@@ -134,7 +133,8 @@ CREATE TABLE `rescue_team` (
 
 INSERT INTO `rescue_team` (`team_id`, `station_id`, `team_name`, `team_username`, `team_password`) VALUES
 (2, 3, 'Shaheen1', 'asd228035', '123'),
-(3, 1, 'Shaheen1', 'sha', '123');
+(3, 1, 'Shaheen1', 'sha', '123'),
+(5, 8, 'township fire engine', 'townshipteam', 'townshipteam');
 
 -- --------------------------------------------------------
 
@@ -151,14 +151,16 @@ CREATE TABLE `staff` (
   `staff_address` varchar(255) NOT NULL,
   `staff_image` varchar(100) NOT NULL,
   `staff_timestamp` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `staff`
 --
 
 INSERT INTO `staff` (`staff_id`, `station_id`, `staff_name`, `father_name`, `staff_cnic`, `staff_address`, `staff_image`, `staff_timestamp`) VALUES
-(4, 3, 'M Faizan', 'Shakoor', '3330368655419', 'Faisalbad ', '', '2022-06-02 08:16:06');
+(7, 8, 'Margoob', 'Ahmad', '31302-21334231-76', 'Lahore', '', '2026-03-16 08:35:01'),
+(8, 8, 'Jahanzab', 'Sha', '73334-1255234-54', 'Brother', '', '2026-03-16 08:35:01'),
+(9, 8, 'Zeshan', 'Musa', '53278-12753-12', 'Boys', '', '2026-03-16 08:35:01');
 
 -- --------------------------------------------------------
 
@@ -176,17 +178,15 @@ CREATE TABLE `station` (
   `station_latitude` varchar(200) NOT NULL,
   `station_longitude` varchar(200) NOT NULL,
   `station_timestamp` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `station`
 --
 
 INSERT INTO `station` (`station_id`, `station_name`, `station_contact`, `station_username`, `station_password`, `station_city`, `station_latitude`, `station_longitude`, `station_timestamp`) VALUES
-(1, 'D Ground Station', 2222, 'dground1', '123', 'faisalabad', '31.40662422379463', '73.10950615723951', '2022-06-02'),
-(3, 'Koh-e-Noor Station ', 112121212, 'kohenoor1', '123', 'Faisalabad', '31.411559638656282', '73.11531799238689', '2022-08-03'),
-(4, 'Samnabad', 2212222, 'samnabad1', '123', 'faisalabad', '31.40521051443001', '73.07919743730821', '2022-06-19'),
-(7, 'GTS Fire Station', 11111, 'gtsfire1', '123', 'faisalabad', '31.41683712032666', '73.09118737244383', '2022-06-24');
+(8, 'Township Station', 2147483647, 'township', 'township', 'lahore', '31.44560383742592', '74.30824892995626 ', '2026-03-16'),
+(9, 'Model Town Station', 2147483647, 'modeltown', 'modeltown', 'lahore', '31.500263402908236', '74.425284818575', '2026-03-16');
 
 --
 -- Indexes for dumped tables
@@ -248,37 +248,37 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `complaint`
 --
 ALTER TABLE `complaint`
-  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `rescue_team`
 --
 ALTER TABLE `rescue_team`
-  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `station`
 --
 ALTER TABLE `station`
-  MODIFY `station_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `station_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- storeimage.php
+++ storeimage.php
@@ -1,22 +1,30 @@
 <?php include 'connection/connectdatabase.php'; ?>
 
 <?php
-    if (!empty($_POST) AND isset($_POST['submit'])) {
+if (!empty($_POST) AND isset($_POST['submit'])) {
   
-  $complaint_latitude=$_POST['complaint_latitude'];
-  $complaint_longitude=$_POST['complaint_longitude'];
-  $contact=$_POST['contact'];
-  $building=$_POST['building'];
-  if (empty($complaint_latitude)) {
-    echo "<script>window.location='complaint.php?error=1'</script>";
-  }
+    $complaint_latitude = $_POST['complaint_latitude'];
+    $complaint_longitude = $_POST['complaint_longitude'];
+    $contact = isset($_POST['contact']) ? $_POST['contact'] : '';
+    $building = isset($_POST['building']) ? $_POST['building'] : '';
+    
+    if (empty($complaint_latitude) || empty($_POST['image'])) {
+        echo "<script>window.location='complaint.php?error=1'</script>";
+        exit;
+    }
 
- $query = mysqli_query($conn,"SELECT * , (3956 * 2 * ASIN(SQRT( POWER(SIN(( $complaint_latitude - station_latitude) *  pi()/180 / 2), 2) +COS( $complaint_latitude * pi()/180) * COS(station_latitude * pi()/180) * POWER(SIN(( $complaint_longitude - station_longitude) * pi()/180 / 2), 2) ))) as distance from station having  distance <= 1000 order by distance");
-$data = mysqli_fetch_assoc($query);
+    // Find nearest station
+    $query = mysqli_query($conn,"SELECT * , (3956 * 2 * ASIN(SQRT( POWER(SIN(( $complaint_latitude - station_latitude) *  pi()/180 / 2), 2) +COS( $complaint_latitude * pi()/180) * COS(station_latitude * pi()/180) * POWER(SIN(( $complaint_longitude - station_longitude) * pi()/180 / 2), 2) ))) as distance from station having distance <= 1000 order by distance");
+    $data = mysqli_fetch_assoc($query);
 
+    if (!$data) {
+        echo "<script>window.location='complaint.php?error=1'</script>";
+        exit;
+    }
 
-$station_id=$data['station_id'];
+    $station_id = $data['station_id'];
 
+    // Handle image
     $img = $_POST['image'];
     $folderPath = "upload/";
   
@@ -29,23 +37,17 @@ $station_id=$data['station_id'];
   
     $file = $folderPath . $fileName;
     file_put_contents($file, $image_base64);
-  
-    // print_r($fileName);
 
-    $insert="INSERT INTO complaint ( station_id, complaint_latitude, complaint_longitude, complaint_image, contact, building ) VALUES ('$station_id','$complaint_latitude','$complaint_longitude','$fileName','$contact','$building')";
+    // Insert complaint
+    $insert = "INSERT INTO complaint (station_id, complaint_latitude, complaint_longitude, complaint_image, contact, building) VALUES ('$station_id','$complaint_latitude','$complaint_longitude','$fileName','$contact','$building')";
 
     if (mysqli_query($conn, $insert)) {
-              echo "New record created successfully";
-              echo "<script>window.location='complaint.php?success=$station_id'</script>";
-            } 
-      else {
-              echo "Error: " . $insert . "<br>" . mysqli_error($conn);
-            }
-            
-        
+        echo "<script>window.location='complaint.php?success=$station_id'</script>";
+    } else {
+        echo "Error: " . $insert . "<br>" . mysqli_error($conn);
+    }
 
-}else{
-              echo "<script>window.location='complaint.php?error=1'</script>";
-
+} else {
+    echo "<script>window.location='complaint.php?error=1'</script>";
 }
-?>  
+?>
